b0d047df30e8b62261100a0e2783e950098cfc62
ibrahim5030 <rionjionn@gmail.com>
2023-05-23 01:03:21 +0600

    codes added to get single car data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,13 @@ async function run() {
 
     app.get('/cars', async(req, res)=>{
       const result = await carsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/cars/:id', async (req, res) => {
+      const id = req.params.id;      
+      const query = { _id: new ObjectId(id) };
+      const result = await carsCollection.findOne(query);
       res.send(result);
     })
 
