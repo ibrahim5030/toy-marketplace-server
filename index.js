0e73ff595b08fdbc9a62955aca20fb01d6c42f68
ibrahim5030 <rionjionn@gmail.com>
2023-05-23 15:19:51 +0600

    code added to delete data from mongodb database

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/cars/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await carsCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
